6ce2ad5379697682840ce1c6dacf55f2d50dc092
cdoig4 <cdoig4@my.bcit.ca>
2022-10-27 09:32:42 -0700

    Create skeleton webapp page & JS for easy use

---
+++ scripts/skeleton.js
@@ -0,0 +1,5 @@
+function loadSkeleton() {
+    console.log($('#navbarplaceholder').load('./skeleton_files/navbar.html'));
+    console.log($('#footerplaceholder').load('./skeleton_files/footer.html'));
+}
+loadSkeleton(); 
