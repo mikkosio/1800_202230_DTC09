b7e18a5d41b1f63d69c66ca6926652992652f653
cdoig4 <cdoig4@my.bcit.ca>
2022-11-06 13:18:02 -0800

    Fix updating all fields with updateMonmon()

--- scripts/monmon.js
+++ scripts/monmon.js
@@ -7,6 +7,7 @@ function insertMonmon() {
                     document.getElementById("creature-name").innerHTML = userDoc.data().monmon;
                     document.getElementById("room-name").innerHTML = userDoc.data().monmon;
                     document.getElementById("monmonimage").src = "./images/" + userDoc.data().monmon + ".gif";
+                    document.getElementById("hatch_status").innerHTML = " hasn't hatched yet... Complete more requests so he/she grows up!"
                 })
         }
         else {
@@ -16,10 +17,26 @@ function insertMonmon() {
 }
 insertMonmon(); //run the function
 
+function updateTasks() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if a user is signed in:
+        if (user) {
+            db.collection("users").doc(user.uid)
+                .onSnapshot(userDoc => {                                                               //arrow notation                 //.data() returns data object
+                    document.getElementById("number_of_tasks").innerHTML = userDoc.data().tasks_completed
+                })
+        }
+        else {
+            console.log("Error: Not a user.")
+        }
+    })
+}
+updateTasks();
+
 const monmonNames = ["Katty", "Poochy", "Croaky"];
 const random = Math.floor(Math.random() * monmonNames.length);
 
-function updateMonMon() {
+function updateMonmon() {
     firebase.auth().onAuthStateChanged(user => {
         // Check if a user is signed in:
         if (user) {
@@ -27,16 +44,20 @@ function updateMonMon() {
                 .onSnapshot(userDoc => {                                                               //arrow notation                 //.data() returns data object
                     var userTasks = userDoc.data().tasks_completed;
                     if (userTasks >= 10) {
-                        db.collection("users").doc(user.uid).update({
-                            monmon: monmonNames[random]
-                        })
-                    }
-                    else {
-                        console.log("Error: Not a user.")
+                        var userMonmon = userDoc.data().monmon;
+                        if (userMonmon == 'Eggy') {
+                            db.collection("users").doc(user.uid).update({
+                                monmon: monmonNames[random]
+                            })
+                        }
+                        else {
+                            document.getElementById("hatch_status").innerHTML = " has hatched! Congratulations on your new friend!";
+                            console.log("Your MonMon has already hatched!")
+                        }
                     }
                 })
         }
     })
 
 }
-once(updateMonMon());
+updateMonmon();
