9568cfb3ad0750c45fdaef6da7b371ddd98eba14
cdoig4 <cdoig4@my.bcit.ca>
2022-11-06 12:33:22 -0800

    Add function to update monmon page on 10 tasks

--- scripts/monmon.js
+++ scripts/monmon.js
@@ -15,3 +15,28 @@ function insertMonmon() {
     })
 }
 insertMonmon(); //run the function
+
+const monmonNames = ["Katty", "Poochy", "Croaky"];
+const random = Math.floor(Math.random() * monmonNames.length);
+
+function updateMonMon() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if a user is signed in:
+        if (user) {
+            db.collection("users").doc(user.uid)
+                .onSnapshot(userDoc => {                                                               //arrow notation                 //.data() returns data object
+                    var userTasks = userDoc.data().tasks_completed;
+                    if (userTasks >= 10) {
+                        db.collection("users").doc(user.uid).update({
+                            monmon: monmonNames[random]
+                        })
+                    }
+                    else {
+                        console.log("Error: Not a user.")
+                    }
+                })
+        }
+    })
+
+}
+once(updateMonMon());
