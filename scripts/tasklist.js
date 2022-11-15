abb67352230fe2fbebb96006c8a8386afcf34e4a
cdoig4 <cdoig4@my.bcit.ca>
2022-11-15 10:04:38 -0800

    Implement task removal and tasks_completed counter

--- scripts/tasklist.js
+++ scripts/tasklist.js
@@ -2,13 +2,33 @@ function bootstrapDelete() {
     $(this).parent().remove();
 };
 
+function editFirestoreTask() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if a user is signed in:
+        if (user) {
+            var tasks_query = db.collection('users').doc(user.uid).collection('tasks').where('TaskTitle', '==', $(this).find(".task-title").text());
+            tasks_query.get().then(function (querySnapshot) {
+                querySnapshot.forEach(function (doc) {
+                    doc.ref.delete();
+                    db.collection("users").doc(user.uid).update({ "tasks_completed": firebase.firestore.FieldValue.increment(1) });
+                });
+            });
+        }
+
+
+        else {
+            // No user is signed in.
+        };
+    })
+};
+
 
 function deleteTask() {
     firebase.auth().onAuthStateChanged(user => {
         // Check if a user is signed in:
         if (user) {
             // Do something for the currently logged-in user here: 
-            var checkBox = document.getElementsByClassName("form-check-input flex-shrink-0")
+            $("body").on("click", "label", editFirestoreTask);
             $("body").on("click", "label", bootstrapDelete);
 
         } else {
