72ac94a3525f0c0e96f2df915d9e9650396cc094
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2022-11-16 10:05:47 -0800

    Refactor time deadline to display remaining time

--- scripts/populatetask.js
+++ scripts/populatetask.js
@@ -4,19 +4,18 @@ function populateCardsDynamically() {
 
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
-            console.log(123)
             db.collection("users").doc(user.uid).collection("tasks").get()
                 .then(allTasks => {
                     allTasks.forEach(doc => {
                         var taskTitle = doc.data().TaskTitle; //gets the task title field
                         var taskDescription = doc.data().TaskDescription; //gets the task description field
                         var dateDeadline = doc.data().DateDeadline; //gets the date deadline field
-                        var timeDeadline = doc.data().TimeDeadline; // gets the time deadline field
+                        var remainingTime = doc.data().RemainingTime; // gets the time deadline field
                         let testTaskList = taskItemTemplate.content.cloneNode(true);
                         testTaskList.querySelector('.task-title').innerHTML = taskTitle;     //equiv getElementByClassName
                         testTaskList.querySelector('.task-description').innerHTML = taskDescription;  //equiv getElementByClassName
                         testTaskList.querySelector('.date-deadline').innerHTML = dateDeadline;  //equiv getElementByClassName
-                        testTaskList.querySelector('.time-deadline').innerHTML = timeDeadline;  //equiv getElementByClassName
+                        testTaskList.querySelector('.time-deadline').innerHTML = remainingTime;  //equiv getElementByClassName
                         taskList.prepend(testTaskList);
                     })
 
