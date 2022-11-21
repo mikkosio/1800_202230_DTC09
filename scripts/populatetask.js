f14575a3bd6ae88f0b555f88354153304407f0ae
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2022-11-20 22:10:34 -0800

    Refactor date format

--- scripts/populatetask.js
+++ scripts/populatetask.js
@@ -7,16 +7,13 @@ function populateCardsDynamically() {
             db.collection("users").doc(user.uid).collection("tasks").get()
                 .then(allTasks => {
                     allTasks.forEach(doc => {
-                        let datedeadlineTemp = userDoc.data().DateDeadline
+                        let datedeadlineTemp = doc.data().FullDeadline
                         let array = datedeadlineTemp.split('-')
 
                         var taskTitle = doc.data().TaskTitle; //gets the task title field
                         var taskDescription = doc.data().TaskDescription; //gets the task description field
-                        var dateDeadline = calculateDate(new Date(array[0], array[1], array[2], array[3], array[4])); //gets the date deadline field
-                        var remainingTime = doc.data().RemainingTime; // gets the time deadline field
-
-                     
-                        let dateDeadlineInMs = 
+                        var dateDeadline = doc.data().DisplayDeadline; //gets the date deadline field
+                        var remainingTime = calculateDate(new Date(array[0], array[1], array[2], array[3], array[4])); // gets the time deadline field
 
                         let testTaskList = taskItemTemplate.content.cloneNode(true);
                         testTaskList.querySelector('.task-title').innerHTML = taskTitle;     //equiv getElementByClassName
