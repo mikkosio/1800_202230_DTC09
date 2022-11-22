4c51d128598e35c30d9b71fe0b54b97d20ae8a5a
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2022-11-21 20:21:33 -0800

    Add task status btn injection to task list

--- scripts/populatetask.js
+++ scripts/populatetask.js
@@ -4,7 +4,9 @@ function populateCardsDynamically() {
 
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
-            db.collection("users").doc(user.uid).collection("tasks").get()
+            db.collection("users").doc(user.uid).collection("tasks")
+                .orderBy("RemainingTime")
+                .get()
                 .then(allTasks => {
                     allTasks.forEach(doc => {
                         let datedeadlineTemp = doc.data().FullDeadline
@@ -13,13 +15,30 @@ function populateCardsDynamically() {
                         var taskTitle = doc.data().TaskTitle; //gets the task title field
                         var taskDescription = doc.data().TaskDescription; //gets the task description field
                         var dateDeadline = doc.data().DisplayDeadline; //gets the date deadline field
-                        var remainingTime = calculateDate(new Date(array[0], array[1], array[2], array[3], array[4])); // gets the time deadline field
+                        var timeRemainingInMs = new Date(array[0], array[1], array[2], array[3], array[4])
+                        var remainingTime = calculateDate(timeRemainingInMs); // gets the time deadline field
+
+                        if (timeRemainingInMs < 0) {
+                            remainingTime = "0";
+                        }
+                        
 
                         let testTaskList = taskItemTemplate.content.cloneNode(true);
                         testTaskList.querySelector('.task-title').innerHTML = taskTitle;     //equiv getElementByClassName
                         testTaskList.querySelector('.task-description').innerHTML = taskDescription;  //equiv getElementByClassName
                         testTaskList.querySelector('.date-deadline').innerHTML = dateDeadline;  //equiv getElementByClassName
                         testTaskList.querySelector('.time-deadline').innerHTML = remainingTime;  //equiv getElementByClassName
+
+                        if (remainingTime[0] == '0') {
+                            testTaskList.querySelector('.urgentBtn').innerHTML = (`<button type="button" style = "margin-bottom: 10px;" class="btn btn-danger btn-sm">Urgent</button>`)
+                        } else if ((Number(remainingTime[0])) > 7) {
+                            testTaskList.querySelector('.urgentBtn').innerHTML = (`<button type="button" style = "margin-bottom: 10px;" class="btn btn-warning btn-sm">Urgent</button>`)
+                        } else {
+                            testTaskList.querySelector('.urgentBtn').innerHTML = (`<button type="button" style = "margin-bottom: 10px;" class="btn btn-success btn-sm">Healthy</button>`)
+                        }
+                            
+
+
                         taskList.prepend(testTaskList);
                     })
 
