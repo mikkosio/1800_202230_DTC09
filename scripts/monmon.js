61f5bf8494eae07225785ef74b186cdd38b4c605
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2022-11-25 00:10:16 -0800

    Add documentation to monmon page

--- scripts/monmon.js
+++ scripts/monmon.js
@@ -8,9 +8,9 @@ firebase.auth().onAuthStateChanged(user => {
     if (user) {
         localStorage.setItem("useruid", user.uid)
         currentUser = db.collection("users").doc(user.uid)
-        
+
         //functions to populate tasks on quest of day and monmon 
-        insertMonmon() 
+        insertMonmon()
         updateTasks();
         updateMonmon();
         populateTasks()
@@ -20,27 +20,30 @@ firebase.auth().onAuthStateChanged(user => {
     }
 })
 
+//Inject monmon into html
 function insertMonmon() {
     currentUser
-        .onSnapshot(userDoc => {                                                               
+        .onSnapshot(userDoc => {
             document.getElementById("creature-name").innerHTML = userDoc.data().monmon;
             document.getElementById("room-name").innerHTML = userDoc.data().monmon;
             document.getElementById("monmonimage").src = "./images/" + userDoc.data().monmon + ".gif";
             document.getElementById("hatch_status").innerHTML = " hasn't hatched yet... Complete more requests so he/she grows up!"
-    })
+        })
 }
 
+//Task progress updater to hatch mon mon
 function updateTasks() {
     currentUser
-        .onSnapshot(userDoc => {                                                               
+        .onSnapshot(userDoc => {
             document.getElementById("number_of_tasks").innerHTML = userDoc.data().tasks_completed + "/10 Tasks Complete";
             document.getElementById("progressBar").style.width = `${(userDoc.data().tasks_completed) * 10}%`;
         })
 }
 
+//Updating monmon upon hatching
 function updateMonmon() {
     currentUser
-        .onSnapshot(userDoc => {                                                              
+        .onSnapshot(userDoc => {
             var userTasks = userDoc.data().tasks_completed;
             if (userTasks >= 10) {
                 var userMonmon = userDoc.data().monmon;
@@ -54,27 +57,28 @@ function updateMonmon() {
                     console.log("Your MonMon has already hatched!")
                 }
             }
-    })
+        })
 }
 
+//Populate tasks on the monmon page
 function populateTasks() {
-            let monmonTaskTemplate = document.getElementById("monmonTaskTemplate");
-            let monmonTaskContainer = document.getElementById("monmonTaskContainer")
+    let monmonTaskTemplate = document.getElementById("monmonTaskTemplate");
+    let monmonTaskContainer = document.getElementById("monmonTaskContainer")
 
-            let date = new Date();
-            let taskCounter = 1;
+    let date = new Date();
+    let taskCounter = 1;
 
-            currentUser.collection("tasks")
-                .orderBy("RemainingTime")
-                .limit(1)
-                .get()
-                .then(userDoc => {
-                    userDoc.forEach(task => {
-                        let monmonTaskCard = monmonTaskTemplate.content.cloneNode(true);
+    currentUser.collection("tasks")
+        .orderBy("RemainingTime")
+        .limit(1)
+        .get()
+        .then(userDoc => {
+            userDoc.forEach(task => {
+                let monmonTaskCard = monmonTaskTemplate.content.cloneNode(true);
 
-                        monmonTaskCard.querySelector(".task").innerHTML = task.data().TaskTitle
-                        monmonTaskCard.querySelector(".date").innerHTML = task.data().DisplayDeadline
-                        monmonTaskContainer.appendChild(monmonTaskCard)
+                monmonTaskCard.querySelector(".task").innerHTML = task.data().TaskTitle
+                monmonTaskCard.querySelector(".date").innerHTML = task.data().DisplayDeadline
+                monmonTaskContainer.appendChild(monmonTaskCard)
+            })
         })
-    })
 }
