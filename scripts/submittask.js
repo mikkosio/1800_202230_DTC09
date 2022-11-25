f710e0b20784610d88c6fbfea015224c6032a053
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2022-11-24 22:54:20 -0800

    Add documentation to functions

--- scripts/submittask.js
+++ scripts/submittask.js
@@ -2,29 +2,23 @@ var form = document.getElementById("taskForm");
 function handleForm(event) { event.preventDefault(); }
 form.addEventListener('submit', handleForm);
 
+//Function to initiate submit task
 function submitTask() {
-    var tasktitle = getInputValue('tasktitle');
+    var tasktitle = getInputValue('tasktitle'); 
     var taskdescription = getInputValue('taskdescription');
-
     var datedeadline = getInputValue('endDate').split('-')
-
-
     let dateDeadlineFireBase = `${datedeadline[0]}-${datedeadline[1]}-${datedeadline[2]}-${getInputValue("hour")}-${getInputValue("minute")}`
     let dateDeadlineDisplay = `${datedeadline[0]}-${datedeadline[1]}-${datedeadline[2]}`
-
     let timeRemainingInMs = new Date(datedeadline[0], datedeadline[1] - 1, datedeadline[2], getInputValue("hour"), getInputValue("minute")) - new Date(Date.now())
-
-
 
     firebase.auth().onAuthStateChanged((user) => {
         if (user) {
             // User logged in already or has just logged in.
             var task = db.collection("users").doc(user.uid).collection('tasks').doc(task).set({
-                TaskTitle: tasktitle,
-                TaskDescription: taskdescription,
-                FullDeadline: dateDeadlineFireBase,
-                DisplayDeadline: dateDeadlineDisplay,
-                RemainingTime: timeRemainingInMs
+                TaskTitle: tasktitle, // task title
+                TaskDescription: taskdescription, // taskd description
+                FullDeadline: dateDeadlineFireBase, // full deadline to use to update remaining time on task page 
+                DisplayDeadline: dateDeadlineDisplay, // date formatted to display on HTMl 
             }).then(() => {
                 window.location.href = "taskadded.html";
             })
