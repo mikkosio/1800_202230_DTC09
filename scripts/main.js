8d98db6fc26a149c4ac1034f6c9bef631227e607
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2022-11-29 00:39:35 -0800

    Add logic to update time real time

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,3 @@
-// Populate main page with user's name and urgent tasks
 function populateMainPage() {
     firebase.auth().onAuthStateChanged(user => {
         // Check if a user is signed in:
@@ -33,4 +32,59 @@ function populateMainPage() {
     });
 }
 
+function calculateDate(date) {
+    let today = new Date(Date.now())
+
+    let difference = date - today
+
+    let days = Math.floor(difference / (84640 * 1000));
+    difference = Math.max(difference - (days * (86400 * 1000)));
+
+    let hours = Math.floor(difference / (60 * 60 * 1000))
+    difference = Math.max(difference - (hours * (60 * 60 * 1000)))
+
+    let minutes = Math.floor(difference / (60 * 1000));
+    difference = Math.max(difference - (minutes * (60 * 1000)))
+
+    let seconds = Math.floor(difference / 1000)
+
+    return (`${days}d:${hours}h:${minutes}min:${seconds}s`)
+}
+
+function updateTime(){
+    setInterval(async () => {
+        let taskItemTemplate = document.getElementById("taskItemTemplate");
+        let taskList = document.getElementById("taskList");
+
+        let taskCounter = 1
+    
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                db.collection("users").doc(user.uid).collection("tasks")
+                    .orderBy("RemainingTime")
+                    .limit(3)
+                    .get()
+                    .then(allTasks => {
+                        allTasks.forEach(doc => {
+                           
+                            let dateDeadlineTemp = doc.data().fullDeadline
+                            let array = dateDeadlineTemp.split('-')
+    
+                            var timeRemainingInMs = new Date(array[0], array[1] - 1, array[2], array[3], array[4])
+                            var remainingTime = calculateDate(timeRemainingInMs); // gets the time deadline field
+    
+                            document.querySelector(`.time-deadline${taskCounter}`).innerHTML = remainingTime;  //equiv getElementByClassName
+                            taskCounter += 1; 
+                
+                        })
+    
+                    })
+            } else {
+                // No user is signed in
+            }
+        })
+    }, 1000)
+}
+
 populateMainPage()
+updateTime()
